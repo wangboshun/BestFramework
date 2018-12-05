712f492921ae2350e8fd2f5b0675192991b27f81
wbs <512742341@qq.com>
2018-12-05 11:20:02 +0800

    添加项目文件。

---
+++ BestFramework.Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BestFramework.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    { 
+        [HttpGet("test")]
+        public string Get()
+        {
+            return "test";
+        } 
+    }
+}
